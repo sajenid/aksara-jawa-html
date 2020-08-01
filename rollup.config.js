2260eb3cb168d52c12b105416985332606f99537
Equan Pr <equan.p@gmail.com>
2020-08-01 10:23:36 +0700

    customElement true

--- rollup.config.js
+++ rollup.config.js
@@ -8,13 +8,13 @@ const name = pkg.name
 	.replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
+	input: 'index.js',
 	output: [
 		{ file: pkg.module, 'format': 'es' },
 		{ file: pkg.main, 'format': 'umd', name }
 	],
 	plugins: [
-		svelte(),
+		svelte({customElement: true}),
 		resolve()
 	]
 };
