e12bdc93a49f6757c3ca2a9c7e0f0a0fde197a87
Equan Pr <equan.p@gmail.com>
2020-08-01 10:23:54 +0700

    kode dasar untung kustom tag

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,1 +1,10 @@
-<!-- your code here -->
+<svelte:options tag={null}/>
+
+<script>
+	import { LatinKeAksara } from '@sajenid/aksara.js';
+
+	export let latin = "Aksara HTML";
+</script>
+
+<span>{LatinKeAksara(latin)}</span>
+<slot></slot>
